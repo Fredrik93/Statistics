38f0b641f11868aeb15f8634ccaf70a934930ad5
Fredrik93 <fredrikullman93@gmail.com>
2021-09-20 11:25:45 +0200

    Ex 1 w1

--- week_1_exercise_2.R
+++ week_1_exercise_2.R
@@ -4,5 +4,21 @@
 #step 1: 
 #Define the grid. This means you decide how many points to use in estimating the posterior, and then you make 
 # a list of the parameter values on the grid 
-p_grid <- seq(from = 0, to = 10, length.out=100)
-plot(p_grid)
+p_grid <- seq(from = 0, to = 1, length.out=20)
+
+#Step 2: 
+#Compute the value of the prior at each parameter value on the grid 
+prior <- rep(1,20)
+
+#Step 3: 
+#Compute the likelihood at each parameter value
+likelihood <- dbinom(6, size=9, prob=p_grid)
+
+#Step 4:
+#Compute the unstandardized posterior at each parameter value, by multiplying the prior to the likelihood
+unstandardized.posterior <- likelihood * prior 
+
+#Step 5: 
+#Standardize the posterior by dividing each value by the sum of all values 
+posterior <- unstandardized.posterior / sum(unstandardized.posterior)
+plot(posterior)
